4a36d81fa1f86ccf73229bba232739b896d8ea6d
Justuserik <55851721+Justuserik@users.noreply.github.com>
2020-05-12 16:07:05 +0200

    creation of client as well as server... more classes will follow

---
+++ Onlineshop_Client.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 5/12/2020
+ * @author 
+ */
+
+public class Onlineshop_Client extends Client {
+  
+  public Onlineshop_Client(){
+    super("192.168.178.24",80);
+    }
+  
+  public void processMessage(String pMessage){
+    
+    }         
+} // end of Onlineshop_Client
+
---
+++ Onlineshop_Server.java
@@ -0,0 +1,27 @@
+/**
+ *
+ * Description
+ *
+ * @version 1.0 from 5/12/2020
+ * @author 
+ */
+
+public class Onlineshop_Server extends Server {
+  
+  public Onlineshop_Server(){
+    super(80);
+    }
+  
+  public void processNewConnection(String pClientIP, int pClientPort){
+    
+    }
+
+  public void processMessage(String pClientIP, int pClientPort, String pMessage){
+    
+    }
+
+  public void processClosingConnection(String pClientIP, int pClientPort){
+    
+    }
+} // end of Onlineshop_Server
+
