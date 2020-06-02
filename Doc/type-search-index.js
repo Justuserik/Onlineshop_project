a6a6de5e97334fa111775d071d5763b24fc122ae
Justuserik <55851721+Justuserik@users.noreply.github.com>
2020-06-02 15:41:16 +0200

    creation of doc please ignore

---
+++ Doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Account"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Artikel"},{"p":"<Unnamed>","l":"Bestellungen"},{"p":"<Unnamed>","l":"ClientGuiCommandBased"},{"p":"<Unnamed>","l":"InterfaceForServer"},{"p":"<Unnamed>","l":"Lager"},{"p":"<Unnamed>","l":"Onlineshop_Server"}]
